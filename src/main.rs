b42d954e0d2710dbcfd859c9b75dc01698e24212
norun111 <u.t012807@gmail.com>
2021-11-12 18:39:26 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{App, HttpServer};
+mod routers;
 
-#[get("/{id}/{name}/index.html")]
-async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", name, id)
-}
-
-#[actix_web::main]
+#[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
+    HttpServer::new(|| App::new().configure(routers::routes))
+        .bind("localhost:8000")?
         .run()
         .await
 }
