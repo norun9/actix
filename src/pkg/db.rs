a8711bd188b8e5e19c9919644fa59d95836797d9
norun111 <u.t012807@gmail.com>
2021-11-13 15:12:17 +0900

    fix

--- src/pkg/db.rs
+++ src/pkg/db.rs
@@ -5,7 +5,6 @@ use diesel::prelude::*;
 pub fn db_connection() -> MysqlConnection {
     let config = config::prepare();
     let database_url = &config.db.url;
-    println!("{}", database_url);
     MysqlConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
