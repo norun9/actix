66c08e61b53831660dca9765c37b0545b485c6e5
norun111 <u.t012807@gmail.com>
2021-11-13 16:24:02 +0900

    fix

--- src/post/model.rs
+++ src/post/model.rs
@@ -1,5 +1,5 @@
 use crate::pkg;
-use crate::schema::posts;
+use crate::schema::posts as posts_schema;
 use diesel::{self, prelude::*};
 use serde::{Deserialize, Serialize};
 
@@ -13,25 +13,47 @@ pub struct Post {
 
 /// This represents a post being inserted into the database, without the auto-generated fields
 #[derive(Deserialize, Insertable)]
-#[table_name = "posts"]
+#[table_name = "posts_schema"]
 pub struct InsertPost {
     pub title: String,
     pub body: String,
 }
 
 #[derive(Insertable)]
-#[table_name = "posts"]
-pub struct NewPost<'a> {
-    title: &'a str,
-    body: &'a str,
+#[table_name = "posts_schema"]
+pub struct NewPost {
+    title: String,
+    body: String,
 }
+// pub struct NewPost<'a> {
+//     title: &'a str,
+//     body: &'a str,
+// }
 
 impl Post {
     pub fn index() -> Result<Vec<Self>, pkg::InternalError> {
         let cnn = pkg::db_connection();
-        let posts = posts::dsl::posts
+        let posts = posts_schema::dsl::posts
             .load::<Post>(&cnn)
             .expect("Error loading posts");
         Ok(posts)
+    }
+
+    pub fn find(&self, id: i32) -> Result<Self, pkg::InternalError> {
+        let cnn = pkg::db_connection();
+        let post = posts_schema::dsl::posts
+            .filter(posts_schema::id.eq(id))
+            .first::<Post>(&cnn)
+            .expect("Error loading posts");
+        Ok(post)
+    }
+
+    pub fn create(&self, new_post: NewPost) -> Result<Self, pkg::InternalError> {
+        let cnn = pkg::db_connection();
+        let new_post_id = diesel::insert_into(posts_schema::dsl::posts)
+            .values(&new_post)
+            .execute(&cnn)
+            .expect("Error saving posts") as i32;
+        self.find(new_post_id)
     }
 }
--- src/post/routes.rs
+++ src/post/routes.rs
@@ -1,14 +1,29 @@
 use crate::pkg;
-use crate::post::model::Post;
+use crate::post::model;
 
-use actix_web::{get, web, HttpResponse};
+use actix_web::{format, get, post, web, HttpResponse};
+
+type PostResult = Result<HttpResponse, pkg::InternalError>;
 
 #[get("/posts")]
-async fn index() -> Result<HttpResponse, pkg::InternalError> {
-    let posts = Post::index()?;
+async fn index() -> PostResult {
+    let posts = model::Post::index()?;
     Ok(HttpResponse::Ok().json(posts))
+}
+
+#[get("/posts/{id}")]
+async fn find(id: web::Path<i32>) -> PostResult {
+    let post = model::Post::find(id.into_inner())?;
+    Ok(HttpResponse::Ok().json(post))
+}
+
+#[post("/posts", format = "application/json", data = "<new_post>")]
+pub fn create(new_post: web::Json<model::NewPost>) -> PostResult {
+    let post_id = model::Post::create(&(new_post));
+    Ok(HttpResponse::Ok().json(post_id))
 }
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(index);
+    cfg.service(find);
 }
