d06100c78ebb0f56a5c2c9db18a86c8244ec40a8
norun111 <u.t012807@gmail.com>
2021-11-13 18:44:43 +0900

    definition result type

--- src/post/model.rs
+++ src/post/model.rs
@@ -19,7 +19,7 @@ pub struct InsertPost {
     body: String,
 }
 
-/// This represents a post being updated
+/// This represents a post being updated, including id to identify
 #[derive(Deserialize)]
 pub struct UpdatePost {
     pub id: i32,
@@ -27,8 +27,12 @@ pub struct UpdatePost {
     pub body: String,
 }
 
+type IndexResult = Result<Vec<Self>, pkg::InternalError>;
+type FindResult = Result<Self, pkg::InternalError>;
+type DeleteResult = Result<usize, pkg::InternalError>;
+
 impl Post {
-    pub fn index() -> Result<Vec<Self>, pkg::InternalError> {
+    pub fn index() -> IndexResult {
         let cnn = pkg::db_connection();
         let posts = posts_schema::dsl::posts
             .load::<Post>(&cnn)
@@ -36,7 +40,7 @@ impl Post {
         Ok(posts)
     }
 
-    pub fn find(id: i32) -> Result<Self, pkg::InternalError> {
+    pub fn find(id: i32) -> FindResult {
         let cnn = pkg::db_connection();
         let post = posts_schema::dsl::posts
             .filter(posts_schema::id.eq(id))
@@ -45,7 +49,7 @@ impl Post {
         Ok(post)
     }
 
-    pub fn create(new_post: InsertPost) -> Result<Self, pkg::InternalError> {
+    pub fn create(new_post: InsertPost) -> FindResult {
         let cnn = pkg::db_connection();
         let _ = diesel::insert_into(posts_schema::dsl::posts)
             .values(&new_post)
@@ -59,7 +63,7 @@ impl Post {
         Ok(new_post)
     }
 
-    pub fn update(target: UpdatePost) -> Result<Self, pkg::InternalError> {
+    pub fn update(target: UpdatePost) -> FindResult {
         let cnn = pkg::db_connection();
         let _ = diesel::update(posts_schema::dsl::posts.find(target.id))
             .set((
@@ -71,7 +75,7 @@ impl Post {
         Ok(Post::find(target.id).unwrap())
     }
 
-    pub fn delete(id: i32) -> Result<usize, pkg::InternalError> {
+    pub fn delete(id: i32) -> DeleteResult {
         let cnn = pkg::db_connection();
         let num_deleted = diesel::delete(posts_schema::dsl::posts.find(id))
             .execute(&cnn)
